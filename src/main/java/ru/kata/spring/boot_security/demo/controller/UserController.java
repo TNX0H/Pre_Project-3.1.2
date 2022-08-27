0d86e1b1d6d4dee3efddc504927ee276c05e322b
TNX0H <dimanmityan@yandex.ru>
2022-08-27 15:39:07 +0300

    3.1.2

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,80 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String users(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping("/{id}")
+    public String getUser (@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "user";
+    }
+
+    @GetMapping("/new")
+    public String addUser(User user) {
+        return "create";
+    }
+
+    @PostMapping("/new")
+    public String add(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "create";
+        } else {
+            userService.addUser(user);
+            return "redirect:/";
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable("id") long id) {
+        userService.removeUser(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("edit/{id}")
+    public String updateUser(@PathVariable("id") long id, Model model) {
+        model.addAttribute(userService.getUserById(id));
+        return "edit";
+    }
+
+    @PatchMapping("/edit")
+    public String update(@Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "edit";
+        } else {
+            userService.updateUser(user);
+            return "redirect:/";
+        }
+    }
+
+    @GetMapping("/user")
+    public String pageForUser (Model model, Principal principal) {
+        model.addAttribute("user", userService.getUserByLogin(principal.getName()));
+        return "user";
+    }
+
+    @GetMapping(value = "/login")
+    public String loginPage() {
+        return "login";
+    }
+}
